e92af03831fc11fce229abb96e1b772d93afdbb0
Assanalikhan <210103042@stu.sdu.edu.kz>
2023-12-17 20:26:48 +0600

    4 commit

--- app/src/main/java/com/example/shopyapp/ItemsActivity.kt
+++ app/src/main/java/com/example/shopyapp/ItemsActivity.kt
@@ -2,12 +2,18 @@ package com.example.shopyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
 
+        val itemsList: RecyclerView = findViewById(R.id.itemsList)
+        val items = arrayListOf<Item>()
 
+        items.add(Item(1, "", "", "", "", 100))
+        items.add(Item(2, "", "", "", "", 200))
+        items.add(Item(3, "", "", "", "", 300))
     }
 }
